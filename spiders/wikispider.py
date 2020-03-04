7a1b69a77afc9cb352a29876dbb03cac6549068a
Nicolas Lazo <nlazo@est.frba.utn.edu.ar>
2020-03-04 17:37:25 -0300

    Create first draft of main spider

---
+++ spiders/wikispider.py
@@ -0,0 +1,22 @@
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+
+class WikiSpider(CrawlSpider):
+    name = "WikiSpider"
+    allowed_domains = ['en.wikipedia.com']
+    start_urls = ['https://en.wikipedia.org/wiki/Wikipedia']
+    base_url = 'https://en.wikipedia.org/wiki/Wikipedia'
+    rules = [
+            Rule(
+                LinkExtractor(allow='wiki/'),
+                callback='parse_article', 
+                follow=True
+                )
+            ]
+
+    def parse_article(self, response):
+        print(f'Reached {response.url}.')
+        yield {
+                'Title': response.css('h1.firstHeading::text').get()
+              }
