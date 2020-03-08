eca84163fe4a441e30c5ee325efa2cea54040fbb
Nicolas Lazo <nlazo@est.frba.utn.edu.ar>
2020-03-08 10:10:23 -0300

    Refactor how WikiSpider follows links

--- spiders/wikispider.py
+++ spiders/wikispider.py
@@ -1,21 +1,24 @@
+import scrapy
+import re
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 
-class WikiSpider(CrawlSpider):
+class WikiSpider(scrapy.Spider):
     name = "WikiSpider"
-    allowed_domains = ['en.wikipedia.com']
+    allowed_domains = ['en.wikipedia.org']
     start_urls = ['https://en.wikipedia.org/wiki/Wikipedia']
-    base_url = 'https://en.wikipedia.org/wiki/Wikipedia'
-    rules = [
-            Rule(
-                LinkExtractor(allow='wiki/'),
-                follow=True
-                )
-            ]
 
     def parse(self, response):
-        print(f'Reached {response.url}.')
+        print(f'/\/\/\/\/\/\/\[WIKISPIDER] Reached {response.url}.')
+
+        allowed_links_regex = '(https://en.wikipedia.org)?/wiki/.*'
+        if not re.match(allowed_links_regex, response.url):
+            print(f'/\/\/\/\/\/\/\[WIKISPIDER] ABANDONED {response.url}')
+            # return
+
         yield {
                 'Title': response.css('h1.firstHeading::text').get()
               }
+
+        yield from response.follow_all(css='div.mw-content-ltr a::attr(href)')
