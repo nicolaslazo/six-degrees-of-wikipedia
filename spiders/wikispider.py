5851cc444d2ddb55a2f2534391f99f120ea32cab
Nicolas Lazo <nlazo@est.frba.utn.edu.ar>
2020-03-08 15:00:20 -0300

    Enforce game rules and add depth limit

--- spiders/wikispider.py
+++ spiders/wikispider.py
@@ -1,7 +1,5 @@
 import scrapy
 import re
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
 
 
 class WikiSpider(scrapy.Spider):
@@ -9,16 +7,38 @@ class WikiSpider(scrapy.Spider):
     allowed_domains = ['en.wikipedia.org']
     start_urls = ['https://en.wikipedia.org/wiki/Wikipedia']
 
+    custom_settings = {
+                        'DEPTH_LIMIT': 6 + 1,  # Depth n = n-1 degrees of separation
+                        'DEPTH_PRIORITY': 10
+                      }
+
     def parse(self, response):
         print(f'/\/\/\/\/\/\/\[WIKISPIDER] Reached {response.url}.')
 
-        allowed_links_regex = '(https://en.wikipedia.org)?/wiki/.*'
-        if not re.match(allowed_links_regex, response.url):
+        if self.is_valid_url(response.url) and not self.contains_blacklisted_keyword(response.url):
             print(f'/\/\/\/\/\/\/\[WIKISPIDER] ABANDONED {response.url}')
-            # return
+            return
 
         yield {
                 'Title': response.css('h1.firstHeading::text').get()
               }
 
         yield from response.follow_all(css='div.mw-content-ltr a::attr(href)')
+
+    def is_valid_url(self, url):
+        allowed_links_regex = '(https://en.wikipedia.org)?/wiki/.*'
+        if re.match(allowed_links_regex, url):
+            return True
+
+    def contains_blacklisted_keyword(self, url):
+        blacklisted_keywords = [
+                                    'File:',
+                                    'Wikipedia:',
+                                    'Template:',
+                                    'Talk:',
+                                    'Portal:',
+                                    'Help',
+                                    'Template talk:',
+                                    '#',
+                                    'Main_Page',
+                               ]
