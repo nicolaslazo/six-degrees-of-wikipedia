8e75b707377a2d5bf65cc80f5c003bba2bc7b225
Nicolas Lazo <nlazo@est.frba.utn.edu.ar>
2020-03-14 21:40:34 -0300

    Fix WikiSpider indentation

--- spiders/wikispider.py
+++ spiders/wikispider.py
@@ -1,5 +1,5 @@
-import scrapy
 import re
+import scrapy
 
 
 class WikiSpider(scrapy.Spider):
@@ -8,40 +8,49 @@ class WikiSpider(scrapy.Spider):
     start_urls = ['https://en.wikipedia.org/wiki/Aschegul']  # Arbitrary article placed for debugging purposes
 
     custom_settings = {
-                        'DEPTH_LIMIT': 1,  # Depth n = n-1 degrees of separation
-                        'DEPTH_PRIORITY': 10,
-                          # 'LOG_LEVEL':'INFO'
-                      }
+        'DEPTH_LIMIT': 1,  # Depth n = n-1 degrees of separation
+
+        # Crawling order settings (BFO)
+        'DEPTH_PRIORITY': 1,
+        'SCHEDULER_DISK_QUEUE': 'scrapy.squeues.PickleFifoDiskQueue',
+        'SCHEDULER_MEMORY_QUEUE': 'scrapy.squeues.FifoMemoryQueue',
+
+        # Logging settings
+        # 'LOG_LEVEL':'INFO'
+    }
+
 
     def parse(self, response):
-        print(f'/\/\/\/\/\/\/\[WIKISPIDER] Reached {response.url}.')
+        print(f'[WIKISPIDER] Reached {response.url}.')
 
         if self.contains_blacklisted_keyword(response.url) or not self.is_valid_url(response.url):
-            print(f'/\/\/\/\/\/\/\[WIKISPIDER] ABANDONED {response.url}')
+            print(f'[WIKISPIDER] ABANDONED {response.url}')
             return
 
         yield {
-                'Title': response.css('h1.firstHeading::text').get()
-              }
+            'Title': response.css('h1.firstHeading::text').get(),
+            'URL': response.url
+        }
 
         yield from response.follow_all(css='div.mw-content-ltr a::attr(href)')
+
 
     def is_valid_url(self, url):
         allowed_links_regex = '(https://en.wikipedia.org)?/wiki/.*'
         if re.match(allowed_links_regex, url):
             return True
 
+
     def contains_blacklisted_keyword(self, url):
         blacklisted_keywords = [
-                                    'File:',
-                                    'Wikipedia:',
-                                    'Template:',
-                                    'Talk:',
-                                    'Portal:',
-                                    'Help:',
-                                    'Template talk:',
-                                    '#',
-                                    'Main_Page',
-                               ]
+            'File:',
+            'Wikipedia:',
+            'Template:',
+            'Talk:',
+            'Portal:',
+            'Help:',
+            'Template_talk:',
+            'Main_Page',
+        ]
 
         return any(map(lambda keyword: keyword in url, blacklisted_keywords))
